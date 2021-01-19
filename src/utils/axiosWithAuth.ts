d417e527e1e8a11d2af0d2130a21504baaf7babc
sametweb <sametmutevellioglu@gmail.com>
2021-01-19 07:39:23 -0800

    BACKEND url updated

--- src/utils/axiosWithAuth.ts
+++ src/utils/axiosWithAuth.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosWithAuth = () =>
   axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.REACT_APP_BACKEND,
     withCredentials: true,
   });
 
